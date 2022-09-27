c161f67feca8dc1f67345d0f0a3f762e2c11eb9e
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-09-27 12:06:29 +0900

    Add files via upload

--- AppliedMathematics/lab002/lab002_20213043.r
+++ AppliedMathematics/lab002/lab002_20213043.r
@@ -1,25 +1,22 @@
 mydata = read.csv("st_data.csv")
 
-myrange = range(mydata$midterm)
-mymean = mean(mydata$midterm)
-myvar = var(mydata$midterm)
-mysd = sd(mydata$midterm)
-print(myrange)
-print(as.integer(mymean))
-print(as.integer(myvar))
-print(as.integer(mysd))
-
 mid = mydata$midterm
+
+print(range(mid))
+print(as.integer(mean(mid)))
+print(as.integer(var(mid)))
+print(as.integer(sd(mid)))
+
 breaks = seq(0, 100, by=10)
 mid.cut = cut(mid, breaks, right=FALSE)
 mid.freq = table(mid.cut)
 print(cbind(mid.freq))
 
 bmp(file="aa.bmp")
-hist(mid, right=FALSE, main="Frequency", xlab="Midterm Exam", ylab="Count")
-dev.off()
+hist(mid, right=FALSE, main="Frequency", xlab="Midterm Exam", ylab="Count", col = NULL)
+aa <- dev.off()
 
 bmp(file="bb.bmp")
 fin = mydata$final
 plot(mid, fin, main="Scatter", xlab="Midterm Exam", ylab="Final Exam")
-dev.off()
+bb <- dev.off()
